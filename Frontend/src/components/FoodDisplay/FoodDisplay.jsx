4022a84978f1a049b1689c2fdaabd64f6bced341
ABit908 <amankumar7320028794@gmail.com>
2024-09-22 22:59:34 +0530

    changes

--- Frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ Frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -7,7 +7,7 @@ const FoodDisplay = ({category}) => {
     const {food_list} =useContext(StoreContext)
 
   return (
-    <div className='food-dispay' id='food-Display'>
+    <div className='food-dispay' id="food-Display">
       <h2>Top Dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item,index)=>{
--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Header.css'
 const Header = () => {
+
+  const scrollToMenu = () => {
+    document.getElementById('food-Display').scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className='header'>
         <div className="header-contents">
             <h2>order ur favourite food here</h2>
             <p>Enjoy a world of delicious snacks delivered straight to your door,anytime you crave.</p>
-            <button>View Menu</button>
+            <button onclick={scrollToMenu}> View Menu</button>
         </div>
     </div>
   )
